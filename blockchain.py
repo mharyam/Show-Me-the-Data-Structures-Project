a59cb4be98d8fda01e96ddbc1f56ecf5ace8a17f
Maryam Yusuf <maryam.m.yusuf1802@gmail.com>
2021-06-06 15:32:07 +0100

    add edge case test to block chain

--- blockchain.py
+++ blockchain.py
@@ -16,13 +16,15 @@ class Block:
         return sha_signature
 
 
-class LinkedList:
+class BlockChain:
     def __init__(self):
         self.head = None
+        self.num_of_element = 0
 
     def append(self,  timestamp, data):
         if self.head is None:
             self.head = Block(timestamp, data, previous_hash=self.previous_hash())
+            self.num_of_element += 1
             return
 
         # Move to the tail (the last node)
@@ -31,6 +33,7 @@ class LinkedList:
             node = node.next
 
         node.next = Block(timestamp, data, previous_hash=self.previous_hash())
+        self.num_of_element += 1
         return
 
     def previous_hash(self):
@@ -41,6 +44,24 @@ class LinkedList:
             node = node.next
         return node.hash
 
+    def remove_block(self, value):
+        if self.head is None:
+            return None
+
+        if self.head.data == value:
+            self.head = self.head.next
+            self.num_of_element -= 1
+            return
+
+        node = self.head
+        while node.next:
+            if node.next.data == value:
+                node.next = node.next.next
+                self.num_of_element -= 1
+                return
+            node = node.next
+        raise ValueError("Value not found in the list.")
+
     def __repr__(self):
         node = self.head
         nodes = []
@@ -50,7 +71,7 @@ class LinkedList:
         return "\n".join(nodes)
 
 
-llist = LinkedList()
+llist = BlockChain()
 llist.append(datetime.now(), "MARYAM YSUUF")
 llist.append(datetime.now(), "Rufai YSUUF")
 
@@ -73,5 +94,53 @@ def test_function2():
     return "PASS"
 
 
-test_function1()
+(test_function1())
 print(test_function2())
+
+
+# second edge test case
+b1 = BlockChain()
+b1.append(datetime.now(), "First")
+b1.append(datetime.now(), "Second")
+b1.append(datetime.now(), "Third")
+
+
+def test_block_chain(block_chain):
+    if block_chain.num_of_element == 3:
+        print("PASS")
+    else:
+        print("FAIL")
+
+
+test_block_chain(b1)
+
+# print(b1)  # should print three block data
+
+
+b1.remove_block("Second")
+b1.append(datetime.now(), "Fourth")
+# should print three block data after removing second and adding fourthhree block data after removing second and adding fourth
+
+
+def test_block_chain2(block_chain):
+    if block_chain.num_of_element == 3:
+        print("PASS")
+    else:
+        print("FAIL")
+
+
+test_block_chain(b1)
+
+
+b2 = BlockChain()
+
+# should print empty because there is no block in b2 chain
+def test_block_chain3(block_chain):
+    if block_chain.num_of_element == 0:
+        print("PASS")
+    else:
+        print("FAIL")
+
+
+test_block_chain3(b2)
+
